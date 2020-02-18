bf12c890ce150f9ca5413097c3b57efe6e66b8e7
Nicholas L <nicholas.h.leung@gmail.com>
2020-02-18 12:47:29 -0800

    Refined code for serial listeners

--- Proximity_Sensor_In/Proximity_Sensor_In.ino
+++ Proximity_Sensor_In/Proximity_Sensor_In.ino
@@ -11,29 +11,27 @@ void setup() {
   Serial.begin(9600);
   while(!Sensor.begin()) {
     Serial.println("Proxy Sensor not connected");
-    delay(5000);
+    delay(3000);
   }
   Sensor.setProximityContinuous(true);
-  Sensor.setProximityHz(125);
+  Sensor.setProximityHz(250);
   Sensor.setLEDmA(200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Sensor.setInterrupt(1,false,false,true,false,0,15000);
-  Serial.println(Sensor.getProximity());
+  Sensor.getProximity();
   if(Sensor.getInterrupt() == bit(0)) {
-    Serial.println("High Threshold triggered, ball detected");
     currentTrigger = true;
+    Sensor.clearInterrupt(bit(0));
   }
   else{
-    Sensor.clearInterrupt(bit(0));
-    Serial.println("High threshold not triggered, ball released");
     currentTrigger = false;
     if(currentTrigger == false && prevTrigger == true) {
       ballCount--;
+      Serial.println("PRXYI increment");
     }
   }
   prevTrigger = currentTrigger;
-  Serial.println(ballCount);
 }
